8d65995804a857b9f6297d185c93c3986f6ba445
Junyoung Choi <fluke8259@gmail.com>
2019-01-21 17:59:16 +0900

    Add test for tokenizer

---
+++ packages/remark-parse/types/ts3.1/index.d.ts
@@ -0,0 +1,50 @@
+import {Node, Parent, Position} from 'unist'
+import {Parser, Attacher} from 'unified'
+
+declare class RemarkParser extends Parser {
+  blockMethods: string[]
+  inlineTokenizers: {
+    [key: string]: remarkParse.Tokenizer
+  }
+  inlineMethods: string[]
+}
+
+declare namespace remarkParse {
+  interface Parse extends Attacher {
+    (options: RemarkParseOptions): void
+    Parser: typeof RemarkParser
+  }
+
+  type Parser = RemarkParser
+
+  interface RemarkParseOptions {
+    gfm: boolean
+    commonmark: boolean
+    footnotes: boolean
+    blocks: string[]
+    pedantic: boolean
+  }
+
+  interface Add {
+    (node: Node, parent?: Parent): Node
+    test(): Position
+    reset(node: Node, parent?: Node): Node
+  }
+
+  type Eat = (value: string) => Add
+
+  type Locator = (value: string, fromIndex: number) => number
+
+  interface Tokenizer {
+    (eat: Eat, value: string, silent: true): boolean | void
+    (eat: Eat, value: string): Node | void
+    locator?: Locator
+    onlyAtStart?: boolean
+    notInBlock?: boolean
+    notInList?: boolean
+    notInLink?: boolean
+  }
+}
+declare const remarkParse: remarkParse.Parse
+
+export = remarkParse
---
+++ packages/remark-parse/types/ts3.1/test.ts
@@ -0,0 +1,61 @@
+import unified = require('unified')
+import * as Unist from 'unist'
+import remarkParse = require('remark-parse')
+
+const parseOptions: Partial<remarkParse.RemarkParseOptions> = {
+  gfm: true,
+  pedantic: true
+}
+
+unified().use(remarkParse, parseOptions)
+
+const badParseOptions: Partial<remarkParse.RemarkParseOptions> = {
+  // $ExpectError
+  gfm: 'true'
+}
+
+const locateMention: remarkParse.Locator = (value, fromIndex) => {
+  return value.indexOf('@', fromIndex)
+}
+
+tokenizeMention.notInLink = true
+tokenizeMention.locator = locateMention
+
+function tokenizeMention(
+  eat: remarkParse.Eat,
+  value: string,
+  silent: true
+): boolean | void
+function tokenizeMention(eat: remarkParse.Eat, value: string): Unist.Node | void
+function tokenizeMention(
+  eat: remarkParse.Eat,
+  value: string,
+  silent?: boolean
+): Unist.Node | boolean | void {
+  const match = /^@(\w+)/.exec(value)
+
+  if (match) {
+    if (silent) {
+      return true
+    }
+
+    const add = eat(match[0])
+    return add({
+      type: 'link',
+      url: 'https://social-network/' + match[1],
+      children: [{type: 'text', value: match[0]}]
+    })
+  }
+}
+
+function mentions(this: unified.Processor) {
+  const Parser = this.Parser as typeof remarkParse.Parser
+  const tokenizers = Parser.prototype.inlineTokenizers
+  const methods = Parser.prototype.inlineMethods
+
+  tokenizers.mention = tokenizeMention
+
+  methods.splice(methods.indexOf('text'), 0, 'mention')
+}
+
+const plugin: unified.Attacher = mentions
